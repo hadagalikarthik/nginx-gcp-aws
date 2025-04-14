f2b0a9022db2ebdc9824d77f9ef378eac73a3c97
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-14 08:58:16 +0530

    Run without backend.tf AWS - all creds

--- nap/aws-main.tf
+++ nap/aws-main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = local.aws_region
+  alias = "aws"
 }
 
 provider "kubernetes" {
@@ -52,63 +53,6 @@ provider "kubectl" {
       "get-token",
       "--cluster-name",
       data.terraform_remote_state.eks[0].outputs.cluster_name
-    ]
-  }
-}
-
-provider "google" {
-    region                  = local.region
-}
-
-provider "kubernetes" {
-  host                   = local.host
-  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-  token                  = local.cluster_token
-  alias                  = "gcp"
-  # Optional: Using exec for kubectl authentication
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "gcloud"
-    args = [
-      "container", "clusters", "get-credentials",
-      local.cluster_name,  # Assuming 'local.cluster_name' is set
-      "--region", local.region  # Assuming 'local.cluster_region' is set
-    ]
-  }
-}
-
-provider "helm" {
-  alias                  = "gcp"
-  kubernetes {
-    host                   = local.host
-    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    token                  = local.cluster_token
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "gcloud"
-      args = [
-        "container", "clusters", "get-credentials",
-        local.cluster_name,  # Cluster name
-        "--region", local.region  # Cluster region
-      ]
-    }
-  }
-}
-
-provider "kubectl" {
-    host                    = local.host
-    cluster_ca_certificate  = base64decode(local.cluster_ca_certificate)
-    token                   = local.cluster_token
-    load_config_file        = false
-    alias                  = "gcp"
-    exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "gcloud"
-    args = [
-      "container", "clusters", "get-credentials",
-      local.cluster_name,  # Cluster name
-      "--region", local.region  # Cluster region
     ]
   }
 }
