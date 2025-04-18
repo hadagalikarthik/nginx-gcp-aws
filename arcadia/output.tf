a4b5163f9aec2d114939b1a3239f8e005e6a7969
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-18 11:51:11 +0530

    Run without backend.tf AWS - Policy and Arcadia

--- arcadia/output.tf
+++ arcadia/output.tf
@@ -1,5 +1,5 @@
 output "external_name" {
   description = "The external hostname of NGINX Ingress from NAP"
   # value       = data.terraform_remote_state.nap.outputs.external_name
-  value = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-nap.outputs.external_name : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-nap.outputs.external_name : null
+  value = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-nap[0].outputs.external_name : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-nap[0].outputs.external_name : null
 }
