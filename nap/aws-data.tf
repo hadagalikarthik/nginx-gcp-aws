468502a1c4f4c59787aa22aad94ef0e54fc59e92
Karthik Hadagali <hadagalikarthik@gmail.com>
2025-04-20 23:40:10 +0530

    GCP AWS - AWS Run

--- nap/aws-data.tf
+++ nap/aws-data.tf
@@ -34,43 +34,43 @@ data "kubernetes_service_v1" "aws-nginx-service" {
   }
 }
 
-locals {
-  project_prefix         = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.project_prefix : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra[0].outputs.project_prefix : null
-  build_suffix           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.build_suffix : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra[0].outputs.build_suffix : null
-  host                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_api_server_url : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.cluster_endpoint : null
-  region                 = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.gcp_region : null
-  cluster_ca_certificate = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_ca_certificate : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.kubeconfig-certificate-authority-data : null
-  cluster_name           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_name : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.cluster_name : null
-  cluster_token          = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_access_token : null
-  cidr                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.cidr : null
-  kubectl_provider_alias = var.CLOUD_PROVIDER == "AWS" ? "aws" : "gcp"
-}
+# locals {
+#   project_prefix         = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.project_prefix : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra[0].outputs.project_prefix : null
+#   build_suffix           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.build_suffix : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra[0].outputs.build_suffix : null
+#   host                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_api_server_url : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.cluster_endpoint : null
+#   region                 = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.gcp_region : null
+#   cluster_ca_certificate = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_ca_certificate : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.kubeconfig-certificate-authority-data : null
+#   cluster_name           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_name : var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.cluster_name : null
+#   cluster_token          = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_access_token : null
+#   cidr                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.cidr : null
+#   kubectl_provider_alias = var.CLOUD_PROVIDER == "AWS" ? "aws" : "gcp"
+# }
 
-locals {
-  # project_prefix          = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra.outputs.project_prefix : null
-  # build_suffix            = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra.outputs.build_suffix : null
-  # cluster_name            = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks.outputs.cluster_name : null  # Single definition
-  ebs_csi_driver_role_arn = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.ebs_csi_driver_role_arn : null
-  cluster_endpoint        = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.cluster_endpoint : null
-  aws_region              = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra[0].outputs.aws_region : null
-  # host                    = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks.outputs.cluster_endpoint : null
-  # cluster_ca_certificate  = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks.outputs.kubeconfig-certificate-authority-data : null
-  app                     = var.CLOUD_PROVIDER == "AWS" ? format("%s-nap-%s", local.project_prefix, local.build_suffix) : null
-}
-
-output "external_name" {
-  value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].status.0.load_balancer.0.ingress.0.ip : var.CLOUD_PROVIDER == "AWS" ? data.kubernetes_service_v1.aws-nginx-service[0].status.0.load_balancer.0.ingress.0.hostname : null
-}
-
-output "external_port" {
-  value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].spec.0.port.0.port : var.CLOUD_PROVIDER == "AWS" ? data.kubernetes_service_v1.aws-nginx-service[0].spec.0.port.0.port : null
-}
-
-output "origin_source" {
-    value = "nap"
-}
-
-output "nap_deployment_name" {
-  value = var.CLOUD_PROVIDER == "GCP" ? helm_release.gcp-nginx-plus-ingress[0].name : var.CLOUD_PROVIDER == "AWS" ? helm_release.aws-nginx-plus-ingress[0].name : null
-  sensitive = true
-}
+# locals {
+#   # project_prefix          = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra.outputs.project_prefix : null
+#   # build_suffix            = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra.outputs.build_suffix : null
+#   # cluster_name            = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks.outputs.cluster_name : null  # Single definition
+#   ebs_csi_driver_role_arn = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.ebs_csi_driver_role_arn : null
+#   cluster_endpoint        = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks[0].outputs.cluster_endpoint : null
+#   aws_region              = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.aws-infra[0].outputs.aws_region : null
+#   # host                    = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks.outputs.cluster_endpoint : null
+#   # cluster_ca_certificate  = var.CLOUD_PROVIDER == "AWS" ? data.terraform_remote_state.eks.outputs.kubeconfig-certificate-authority-data : null
+#   app                     = var.CLOUD_PROVIDER == "AWS" ? format("%s-nap-%s", local.project_prefix, local.build_suffix) : null
+# }
+#
+# output "external_name" {
+#   value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].status.0.load_balancer.0.ingress.0.ip : var.CLOUD_PROVIDER == "AWS" ? data.kubernetes_service_v1.aws-nginx-service[0].status.0.load_balancer.0.ingress.0.hostname : null
+# }
+#
+# output "external_port" {
+#   value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].spec.0.port.0.port : var.CLOUD_PROVIDER == "AWS" ? data.kubernetes_service_v1.aws-nginx-service[0].spec.0.port.0.port : null
+# }
+#
+# output "origin_source" {
+#     value = "nap"
+# }
+#
+# output "nap_deployment_name" {
+#   value = var.CLOUD_PROVIDER == "GCP" ? helm_release.gcp-nginx-plus-ingress[0].name : var.CLOUD_PROVIDER == "AWS" ? helm_release.aws-nginx-plus-ingress[0].name : null
+#   sensitive = true
+# }
--- nap/gcp-data.tf
+++ nap/gcp-data.tf
@@ -26,30 +26,30 @@ data "kubernetes_service_v1" "gcp-nginx-service" {
   }
 }
 
-locals {
-  project_prefix         = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.project_prefix : null
-  build_suffix           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.build_suffix :  null
-  host                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_api_server_url : null
-  region                 = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.gcp_region : null
-  cluster_ca_certificate = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_ca_certificate : null
-  cluster_name           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_name : null
-  cluster_token          = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_access_token : null
-  cidr                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.cidr : null
-}
-
-output "external_name" {
-  value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].status.0.load_balancer.0.ingress.0.ip : null
-}
-
-output "external_port" {
-  value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].spec.0.port.0.port : null
-}
-
-output "origin_source" {
-    value = "nap"
-}
-
-output "nap_deployment_name" {
-  value = var.CLOUD_PROVIDER == "GCP" ? helm_release.gcp-nginx-plus-ingress[0].name : null
-  sensitive = true
-}
+# locals {
+#   project_prefix         = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.project_prefix : null
+#   build_suffix           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.build_suffix :  null
+#   host                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_api_server_url : null
+#   region                 = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.gcp_region : null
+#   cluster_ca_certificate = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_ca_certificate : null
+#   cluster_name           = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_name : null
+#   cluster_token          = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gke[0].outputs.kubernetes_cluster_access_token : null
+#   cidr                   = var.CLOUD_PROVIDER == "GCP" ? data.terraform_remote_state.gcp-infra[0].outputs.cidr : null
+# }
+#
+# output "external_name" {
+#   value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].status.0.load_balancer.0.ingress.0.ip : null
+# }
+#
+# output "external_port" {
+#   value = var.CLOUD_PROVIDER == "GCP" ? data.kubernetes_service_v1.gcp-nginx-service[0].spec.0.port.0.port : null
+# }
+#
+# output "origin_source" {
+#     value = "nap"
+# }
+#
+# output "nap_deployment_name" {
+#   value = var.CLOUD_PROVIDER == "GCP" ? helm_release.gcp-nginx-plus-ingress[0].name : null
+#   sensitive = true
+# }
